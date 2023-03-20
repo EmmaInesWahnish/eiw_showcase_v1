a4ce83c73e198808bc9af86ab7ac9e69c7fb3e8d
Emma <emma.wahnish@groovinads.com>
2023-03-20 18:01:50 -0300

    ga styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { route, routes} from 'react-router-dom'
+/** SERVICES */
+import { urlPath } from "./services";
+/** CONTEXT */
+import {
+  GeneralContextProvider,
+} from "./contexts";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
