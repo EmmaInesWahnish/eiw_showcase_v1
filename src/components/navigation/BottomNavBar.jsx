84af2d4112f34d662e599142cc89401702a30eef
Emma <emma.wahnish@groovinads.com>
2023-03-20 21:09:41 -0300

    logo

--- src/components/navigation/BottomNavBar.jsx
+++ src/components/navigation/BottomNavBar.jsx
@@ -1,7 +1,17 @@
+// CONFIGURATION
+import config from "../../config";
+
 const BottomNavBar = () => {
   return (
     <div id="ga-header" className="shadow-2">
       <nav className="container-fluid flex-nowrap justify-content-between navbar navbar-expand-lg">
+        <div className="navbar-brand">
+        <img
+              src={config.logoimg}
+              className="ga-logo"
+              alt="Groovinads Reports logo"
+            />
+        </div>
       </nav>
     </div>
   );
